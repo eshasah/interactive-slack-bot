96f057fb1e234d8fa0fabf45776a9e5f05055d8f
eshasah <esha8sah@gmail.com>
2022-06-27 11:34:24 -0700

    initial commit

---
+++ bot.py
@@ -0,0 +1,12 @@
+from pydoc import cli
+import slack
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
+
+client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
+client.chat_postMessage(channel='#interactive-bot', text="Hello world!")
+
